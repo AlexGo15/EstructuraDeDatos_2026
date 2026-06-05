2e08deba8782ba51ea10cab3254b08467fbe1a34
AlexGo15 <alexxgo1504@gmail.com>
2026-06-04 21:10:01 -0600

    Clase 1: Implementación de funciones y lógica de polígonos

--- Practica_1/Program.cs
+++ Practica_1/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        double numero = LeerDoublePositivo();
+        Console.WriteLine($"Número válido ingresado: {numero}");
+    }
+
+    static double LeerDoublePositivo()
+    {
+        double valor;
+        do
+        {
+            Console.Write("Ingrese un número decimal positivo: ");
+            string? entrada = Console.ReadLine();
+
+            if(double.TryParse(entrada, out valor) && valor >0)
+            {
+                return valor;
+            }
+            Console.WriteLine("Error: favor de ingresar un número decimal positivo válido.\n");
+        }
+        while (true);
+    }
+}
